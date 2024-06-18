a4730ebaaf2a8d10b3e0066017a9a53c7aaca1cc
MilanMareels <milan.mareels@gmail.com>
2024-06-18 19:51:52 +0200

    fixed regsiter

--- src/api/users/register.ts
+++ src/api/users/register.ts
@@ -1,10 +1,10 @@
 import express from "express";
-import bycrypt from 'bcrypt';
-import { v4 as uuidv4 } from 'uuid';
+import bycrypt from "bcrypt";
+import { v4 as uuidv4 } from "uuid";
 
 import { createResponseObject, handleErrors, isNullOrUndefined, validateBody } from "../../common/common";
 import { queryUserByEmailOrUserName } from "../../database/Users/queryUserByEmailOrUserName";
-import { ConflictError, UnprocessableContentError } from "../../errors/error";
+import { ConflictError, NotFoundError, UnprocessableContentError } from "../../errors/error";
 import { User } from "../../types/User/User";
 import { queryAddUser } from "../../database/Users/queryAddUser";
 import validate from "deep-email-validator";
@@ -13,49 +13,58 @@ import { errorMessages } from "../../errors/errorMessages";
 const router = express.Router();
 
 router.post("/register", async (req, res) => {
-    try {
-        const { userName, fullName, email, password } = validateBody(req, ['userName', 'fullName', 'email', 'password']);
+  try {
+    const { userName, fullName, email, password } = validateBody(req, ["userName", "fullName", "email", "password"]);
 
-        await validateNewUser(email, userName);
-        await validateEmail(email);
+    await validateNewUser(email, userName);
+    await validateEmail(email);
 
-        const newUser: User = await createNewUser(userName, fullName, email, password);
+    const newUser: User = await createNewUser(userName, fullName, email, password);
 
-        await queryAddUser(newUser);
+    await queryAddUser(newUser);
 
-        return createResponseObject(201, { message: 'Registration successful!' }, res);
-    } catch (error) {
-        return handleErrors(error, res);
-    }
+    const user: User = await findUser(email, userName);
+
+    return createResponseObject(201, user, res);
+  } catch (error) {
+    return handleErrors(error, res);
+  }
 });
 
 const validateNewUser = async (email: string, userName: string) => {
-    const user = await queryUserByEmailOrUserName(email, userName);
+  const user = await queryUserByEmailOrUserName(email, userName);
 
-    if (!isNullOrUndefined(user)) throw new ConflictError(errorMessages.userAlreadyExist);
+  if (!isNullOrUndefined(user)) throw new ConflictError(errorMessages.userAlreadyExist);
 };
 
 const validateEmail = async (email: string): Promise<void> => {
-    const validateEmail = await validate({ email: email, validateSMTP: false });
-    const isEmailValid: boolean = validateEmail.valid;
+  const validateEmail = await validate({ email: email, validateSMTP: false });
+  const isEmailValid: boolean = validateEmail.valid;
 
-    if (!isEmailValid) throw new UnprocessableContentError(errorMessages.invalidEmail);
+  if (!isEmailValid) throw new UnprocessableContentError(errorMessages.invalidEmail);
 };
 
 const hashPassword = async (password: string) => {
-    return await bycrypt.hash(password, 10);
+  return await bycrypt.hash(password, 10);
 };
 
 const createNewUser = async (userName: string, fullName: string, email: string, password: string): Promise<User> => {
-    return {
-        userId: uuidv4(),
-        userName: userName,
-        fullName: fullName,
-        email: email,
-        password: await hashPassword(password),
-        accountCreated: new Date().getTime()
-    }
+  return {
+    userId: uuidv4(),
+    userName: userName,
+    fullName: fullName,
+    email: email,
+    password: await hashPassword(password),
+    accountCreated: new Date().getTime(),
+  };
+};
+
+const findUser = async (email: string, userName: string): Promise<User> => {
+  const user = (await queryUserByEmailOrUserName(email, userName)) as User;
+
+  if (isNullOrUndefined(user)) throw new NotFoundError(errorMessages.userNotFound);
+
+  return user;
 };
 
 export default router;
-
