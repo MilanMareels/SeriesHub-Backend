172f8d097f1ceea79f93f45a7aba9ca3a4c2ff76
MilanMareels <milan.mareels@gmail.com>
2024-08-14 15:32:05 +0200

    fixed bug in code

--- src/api/anime/getAllAnimeSeriesOfUser.ts
+++ src/api/anime/getAllAnimeSeriesOfUser.ts
@@ -11,39 +11,39 @@ import { errorMessages } from "../../errors/errorMessages";
 const router = express.Router();
 
 router.get("/user/:userId/AnimeSeries", async (req, res) => {
-	const userId: string = req.params.userId;
-	const page = parseInt(req.query.page as string) || 1;
-	const listStatus: string = req.query.listStatus!.toString();
+  const userId: string = req.params.userId;
+  const page = parseInt(req.query.page as string) || 1;
+  const listStatus: string = req.query.listStatus as string;
 
-	const definedListStatus: string = defineListStatus(listStatus);
-	try {
-		await validateUser(userId);
+  const definedListStatus: string = defineListStatus(listStatus);
+  try {
+    await validateUser(userId);
 
-		const animeSeries: AnimeSerie[] = (await queryAllAnimeSeries(userId, page, definedListStatus)) as AnimeSerie[];
+    const animeSeries: AnimeSerie[] = (await queryAllAnimeSeries(userId, page, definedListStatus)) as AnimeSerie[];
 
-		checkIfUserHasAnimeSeries(animeSeries, definedListStatus);
+    checkIfUserHasAnimeSeries(animeSeries, definedListStatus);
 
-		return createResponseObject(200, animeSeries, res);
-	} catch (error) {
-		return handleErrors(error, res);
-	}
+    return createResponseObject(200, animeSeries, res);
+  } catch (error) {
+    return handleErrors(error, res);
+  }
 });
 
 const defineListStatus = (listStatus: string): string => {
-	switch (listStatus) {
-		case "1":
-			return listStatusEnum[1];
-		case "2":
-			return listStatusEnum[2];
-		case "3":
-			return listStatusEnum[3];
-		default:
-			return "0";
-	}
+  switch (listStatus) {
+    case "1":
+      return listStatusEnum[1];
+    case "2":
+      return listStatusEnum[2];
+    case "3":
+      return listStatusEnum[3];
+    default:
+      return "0";
+  }
 };
 
 const checkIfUserHasAnimeSeries = (animeSeries: AnimeSerie[], definedListStatus: string) => {
-	if (animeSeries.length <= 0) throw new NotFoundError(errorMessages.userHasNoAnimeSeries(definedListStatus));
+  if (animeSeries.length <= 0) throw new NotFoundError(errorMessages.userHasNoAnimeSeries(definedListStatus));
 };
 
 export default router;
