778a19a4f1473a3fc3d2629cc0c28c88e54d98a1
MilanMareels <milan.mareels@gmail.com>
2024-08-15 12:59:35 +0200

    filer and search done

--- src/api/anime/getAllAnimeSeriesOfUser.ts
+++ src/api/anime/getAllAnimeSeriesOfUser.ts
@@ -7,19 +7,30 @@ import { AnimeSerie } from "../../types/Anime/AnimeSerie";
 import { validateUser } from "../../checks/users/userChecks";
 import { NotFoundError } from "../../errors/error";
 import { errorMessages } from "../../errors/errorMessages";
+import { Query } from "../../types/Anime/Query";
 
 const router = express.Router();
 
 router.get("/user/:userId/AnimeSeries", async (req, res) => {
   const userId: string = req.params.userId;
   const page = parseInt(req.query.page as string) || 1;
-  const listStatus: string = req.query.listStatus as string;
+  const listStatus: string = (req.query.listStatus as string) || "2";
+  const animeTitle: string = req.query.search as string;
 
   const definedListStatus: string = defineListStatus(listStatus);
   try {
     await validateUser(userId);
 
-    const animeSeries: AnimeSerie[] = (await queryAllAnimeSeries(userId, page, definedListStatus)) as AnimeSerie[];
+    let query: Query = {
+      userId: userId,
+      listStatus: definedListStatus,
+    };
+
+    if (animeTitle) {
+      query["animeTitle"] = { $regex: animeTitle, $options: "i" };
+    }
+
+    const animeSeries: AnimeSerie[] = (await queryAllAnimeSeries(query, page)) as AnimeSerie[];
 
     checkIfUserHasAnimeSeries(animeSeries, definedListStatus);
 
