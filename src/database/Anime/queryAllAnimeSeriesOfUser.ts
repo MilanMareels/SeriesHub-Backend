2173ab920f85b803a594adf43dace4bcf03080fc
MilanMareels <125263673+MilanMareels@users.noreply.github.com>
2024-08-15 13:02:42 +0200

    filer and search done (#9)

--- src/database/Anime/queryAllAnimeSeriesOfUser.ts
+++ src/database/Anime/queryAllAnimeSeriesOfUser.ts
@@ -2,25 +2,26 @@ import { connectDatabase, closeDatabase } from "../db";
 import { MongoClient } from "mongodb";
 import "dotenv/config";
 import { AnimeSerie } from "../../types/Anime/AnimeSerie";
+import { Query } from "../../types/Anime/Query";
 
 const uri: string = process.env.MONGO_CONNECT_URL!;
 const database: string = process.env.DATABASE!;
 const client = new MongoClient(uri);
 
-export const queryAllAnimeSeries = async (userId: string, page: number, listStatus: string): Promise<AnimeSerie[] | unknown> => {
-	const itemsPerPage = 10;
-	try {
-		await connectDatabase();
-		return await client
-			.db(database)
-			.collection("AnimeSeries")
-			.find({ userId: userId, listStatus: listStatus })
-			.skip((page - 1) * itemsPerPage)
-			.limit(itemsPerPage)
-			.toArray();
-	} catch (error) {
-		return error;
-	} finally {
-		await closeDatabase();
-	}
+export const queryAllAnimeSeries = async (query: Query, page: number): Promise<AnimeSerie[] | unknown> => {
+  const itemsPerPage = 10;
+  try {
+    await connectDatabase();
+    return await client
+      .db(database)
+      .collection("AnimeSeries")
+      .find(query)
+      .skip((page - 1) * itemsPerPage)
+      .limit(itemsPerPage)
+      .toArray();
+  } catch (error) {
+    return error;
+  } finally {
+    await closeDatabase();
+  }
 };
